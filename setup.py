aaa43299da9557c0d7a793291b0812f49de4c3c7
Callum Booth <callumwbooth@gmail.com>
2011-08-18 16:30:30 +0100

    version 1.0.1 setup

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name='Pyks',
-      version='1.0.0',
+      version='1.0.1',
       description='Python kilo-seconds manipulation module',
       author='Callum Booth',
       author_email='callumwbooth@gmail.com',
-	  url='https://github.com/cbooth/pyks',
+	  url='https://cbooth.github.com/pyks',
       package=['pyks']
      )
